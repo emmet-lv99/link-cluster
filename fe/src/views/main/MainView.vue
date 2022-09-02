3703230975898ecf3836b8c780f673fb06195741
emmet-lv99 <dachanisfull@gmail.com>
2022-09-02 09:24:09 +0900

    ~ing UI publishing

--- fe/src/views/main/MainView.vue
+++ fe/src/views/main/MainView.vue
@@ -1,5 +1,5 @@
 <template>
-  <section id="main-view" class="min-h-screen">
+  <section id="main-view" class="min-h-screen bg-purple-100">
     <header class="main__header h-20 p-4 flex justify-between bg-slate-800">
       <div class="utill-buttons__wrapper">
         <el-button class="h-full" type="info" plain>미리보기</el-button>
@@ -11,6 +11,46 @@
         </el-button>
       </nav>
     </header>
+    <section class="main__body">
+      <section class="main__body-header">
+        <div
+          class="main__body-header-cover-img w-full relative before:content-[''] before:pt-56per before:block before:bg-blue-500 overflow-hidden"
+        >
+          <img
+            class="absolute top-0 left-1/2 translate-x-minus1/2 w-full h-full object-cover"
+            src="../../assets/cat.jpg"
+            alt="cover image"
+          />
+        </div>
+      </section>
+      <section class="main__body-main-content relative w-full">
+        <div class="content-wrapper top-[-40px] absolute p-2.5 w-full">
+          <div
+            class="main__body-header-profile p-4 rounded-2xl bg-main-profile-bg backdrop-blur-3xl"
+          >
+            <div class="main__body-header-profile-data flex items-center pb-4">
+              <div
+                class="profile-user__image overflow-hidden w-16 h-16 rounded-full"
+              >
+                <img
+                  class="w-full h-full object-cover"
+                  src="../../assets/cat.jpg"
+                  alt="프로필 이미지"
+                />
+              </div>
+              <div class="profile-user__user-data ml-2.5">
+                <div class="user-data__id">user_id</div>
+                <div class="user-data__url">user.url/path</div>
+              </div>
+            </div>
+            <div class="main__body-header-profile-edits flex">
+              <el-button class="flex-1">프로필 수정</el-button>
+              <el-button class="flex-1">디자인 선택</el-button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </section>
   </section>
   <el-drawer v-model="drawer" title="I am the title" :with-header="false">
     <span>Hi there!</span>
--- fe/tailwind.config.cjs
+++ fe/tailwind.config.cjs
@@ -5,7 +5,17 @@ module.exports = {
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      translate: {
+        'minus1/2': '-50%'
+      },
+      padding: {
+        '56per' : '56%',
+      },
+      colors: {
+        'main-profile-bg': 'rgba(255, 255, 255, .7)',
+      }
+    }
   },
   plugins: [],
 }
